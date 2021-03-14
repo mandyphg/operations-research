50003d7ce65d77726de50e35f6a61c207071d114
mandyphg <44054536+mandyphg@users.noreply.github.com>
2021-03-14 21:57:57 +0800

    Update Shift Optimization Python.py

--- Shift Optimization/Shift Optimization Python.py
+++ Shift Optimization/Shift Optimization Python.py
@@ -2,3 +2,19 @@ import pandas as pd
 import datetime
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, inspect, DateTime, Float
 from amplpy import AMPL, DataFrame, Environment
+
+# Connect to DB
+serverDB = 'mssql://name_of_server?trusted_connection=yes;driver=SQL+Server+Native+Client+11.0'
+engineDB = create_engine(serverDB).connect()
+
+# retrieve staff requirement for the day from Database
+query= """
+SELECT *
+  FROM [name_of_table]
+ WHERE operation_date = ...
+"""
+
+staff_df = pd.read_sql(query, engineDB)
+staff_df['HalfHour'] = staff_df['HalfHour'] * 10
+staff_df['HalfHour'] = "H" + staff_df['HalfHour'].astype(int).astype(str).str.zfill(3)
+staff_df_sum = staff_df.groupby(['HalfHour'])['StaffRequired'].sum()
